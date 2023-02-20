e4f40216ad2c85bff83c27eced3ec5924b9cb53e
Hieu Nguyen Duc <isaactesla@gmail.com>
2023-02-20 18:17:18 +0700

    update navbar padding

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ import NavItem from './NavItem';
 function NavBar() {
   return (
     <nav className="fixed top-0 z-50 w-full border-b border-gray-700 bg-gray-800">
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
+      <div className="container mx-auto flex flex-wrap items-center justify-between p-2">
         <a href="https://flowbite.com/" className="flex items-center">
           <img src={`${import.meta.env.BASE_URL}logo.svg`} className="mr-3 h-9" alt="SQC Logo" />
           <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
