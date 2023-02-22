4a92a62b33e3e9a0020cb30ddc0a69a59c4d0460
Hieu Nguyen Duc <isaactesla@gmail.com>
2023-02-22 05:41:33 +0000

    manage question states

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 
 import Editor from '@/components/Editor';
 import NavBar from '@/components/NavBar';
 import SideBar from '@/components/SideBar';
+import QuestionsContext from '@/contexts/questionsContext';
+import { initialQuestions, questionsReducer } from '@/reducers/questionReducer';
 
 function App() {
   const [value, setValue] = useState('');
+  const [questions, questionsDispatch] = useReducer(
+    questionsReducer,
+    initialQuestions,
+  );
 
   return (
     <div className="App">
-      <NavBar />
-      <SideBar />
-      <div className="ml-64 p-4">
-        <div className="mt-28 rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700 lg:mt-16">
-          <Editor value={value} onChange={setValue} />
-          <h2 className="my-4 text-xl font-bold">Output:</h2>
-          <div className="w-full bg-slate-400">
-            <code>{value}</code>
+      <QuestionsContext.Provider value={{ questions, questionsDispatch }}>
+        <NavBar />
+        <SideBar />
+        <div className="ml-64 p-4">
+          <div className="mt-28 rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700 lg:mt-16">
+            <Editor value={value} onChange={setValue} />
+            <h2 className="my-4 text-xl font-bold">Output:</h2>
+            <div className="w-full bg-slate-400">
+              <code>{value}</code>
+            </div>
           </div>
         </div>
-      </div>
+      </QuestionsContext.Provider>
     </div>
   );
 }
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import QuestionsContext from '@/contexts/questionsContext';
+import { Actions } from '@/reducers/questionReducer';
 
 import SideBarButton from './SideBarButton';
 
 function SideBar() {
+  const { questions, questionsDispatch } = useContext(QuestionsContext);
+  const addQuestion = () => {
+    questionsDispatch({ type: Actions.ADD });
+  };
   return (
     <aside
       className="fixed top-0 left-0 z-40 h-screen w-64 border-r border-gray-700 bg-gray-800 pt-32 transition-transform lg:pt-20"
@@ -10,11 +17,10 @@ function SideBar() {
     >
       <div className="h-full overflow-y-auto bg-gray-800 px-3 pb-4">
         <ul className="grid grid-cols-3 gap-4">
-          <SideBarButton label="1" />
-          <SideBarButton label="22" />
-          <SideBarButton label="333" />
-          <SideBarButton label="4444" />
-          <SideBarButton label="+" />
+          {questions.map((_, index) => (
+            <SideBarButton key={index} label={index + 1} />
+          ))}
+          <SideBarButton label="+" onClick={() => addQuestion()} />
         </ul>
       </div>
     </aside>
