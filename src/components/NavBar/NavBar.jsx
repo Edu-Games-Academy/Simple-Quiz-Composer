d0a955c12aa84b57c27ca822a8d350b8f9bcaf23
Hieu Nguyen Duc <isaactesla@gmail.com>
2023-02-22 08:04:24 +0000

    fix width class navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,11 @@ function NavBar() {
     <nav className="fixed top-0 z-50 w-full border-b border-gray-700 bg-gray-800">
       <div className="container mx-auto flex flex-wrap items-center justify-between p-2">
         <a href="https://flowbite.com/" className="flex items-center">
-          <img src={`${import.meta.env.BASE_URL}logo.svg`} className="mr-3 h-9" alt="SQC Logo" />
+          <img
+            src={`${import.meta.env.BASE_URL}logo.svg`}
+            className="mr-3 h-9"
+            alt="SQC Logo"
+          />
           <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
             Simple Quiz Composer
           </span>
@@ -16,7 +20,7 @@ function NavBar() {
         <div className="order-2 flex">
           <ExportJsonButton />
         </div>
-        <div className="order-1 flex w-full w-auto items-center justify-between">
+        <div className="order-1 flex w-auto items-center justify-between">
           <ul className="mt-0 flex flex-row space-x-8 rounded-lg border-0 bg-gray-800 p-4 text-sm font-medium">
             <NavItem>Import JSON</NavItem>
             <NavItem>
