c822d8c1f21ede2745086c27af0476c77d8a12a8
Hieu Nguyen <isaactesla@gmail.com>
2023-02-24 12:11:53 +0700

    sidebar to set selected question

--- src/components/SideBar/SideBarButton.jsx
+++ src/components/SideBar/SideBarButton.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function SideBarButton({ label, onClick }) {
+function SideBarButton({ label, onClick, isSelected }) {
   return (
     <li>
       <button
         onClick={onClick}
-        className="w-full rounded-lg border border-gray-500 p-2 text-center text-white hover:bg-gray-700"
+        aria-selected={isSelected}
+        className="w-full rounded-lg border border-gray-500 p-2 text-center text-white hover:bg-gray-700 aria-selected:border-none aria-selected:bg-gradient-to-r aria-selected:from-blue-500 aria-selected:via-blue-600 aria-selected:to-blue-700"
       >
         {label}
       </button>
@@ -17,6 +18,7 @@ function SideBarButton({ label, onClick }) {
 SideBarButton.propTypes = {
   label: PropTypes.string,
   onClick: PropTypes.func,
+  isSelected: PropTypes.bool,
 };
 
 export default SideBarButton;
