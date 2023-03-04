b04ee936c06a3ae2d6b3f8994810c17e44fc5f5a
Hieu Nguyen <isaactesla@gmail.com>
2023-03-04 11:05:45 +0700

    add icons for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { ReactComponent as UploadIcon } from '@/assets/svg/upload.svg';
+import { ReactComponent as UploadFileIcon } from '@/assets/svg/upload_file.svg';
+
 import ExportJsonButton from './ExportJsonButton';
 import NavItem from './NavItem';
 
@@ -22,10 +25,26 @@ function NavBar() {
         </div>
         <div className="order-1 flex w-auto items-center justify-between">
           <ul className="mt-0 flex flex-row space-x-8 rounded-lg border-0 bg-gray-800 p-4 text-sm font-medium">
-            <NavItem>Import JSON</NavItem>
             <NavItem>
-              Import Moddle GIFT{' '}
-              <span className="mr-2 rounded border border-blue-400 bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800 dark:bg-gray-700 dark:text-blue-400">
+              <UploadIcon
+                aria-hidden="true"
+                className="mr-2 -ml-1 h-4 w-4"
+                fill="currentColor"
+                focusable="false"
+                role="img"
+              />
+              Import JSON
+            </NavItem>
+            <NavItem>
+              <UploadFileIcon
+                aria-hidden="true"
+                className="mr-2 -ml-1 h-4 w-4"
+                fill="currentColor"
+                focusable="false"
+                role="img"
+              />
+              Import Moddle GIFT
+              <span className="ml-1 rounded border border-blue-400 bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800 dark:bg-gray-700 dark:text-blue-400">
                 Beta
               </span>
             </NavItem>
