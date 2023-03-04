39c9db311a72a61b360c464ea09277846f5493f5
Hieu Nguyen <isaactesla@gmail.com>
2023-03-04 14:17:34 +0700

    format raw data

--- src/components/RawQuestion/RawQuestion.jsx
+++ src/components/RawQuestion/RawQuestion.jsx
@@ -1,14 +1,23 @@
+import hljs from 'highlight.js';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 function RawQuestion({ question }) {
   return (
     <div>
-      <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-        Question
+      <h5 className="text-xl font-semibold tracking-tight text-gray-900">
+        Code:
       </h5>
-      <div className="w-full bg-slate-400">
-        <code>{question.question}</code>
+      <div className="w-full overflow-scroll rounded-sm bg-slate-700 p-2">
+        <pre>
+          <code
+            dangerouslySetInnerHTML={{
+              __html: hljs.highlight(JSON.stringify(question, null, 2), {
+                language: 'json',
+              }).value,
+            }}
+          />
+        </pre>
       </div>
     </div>
   );
