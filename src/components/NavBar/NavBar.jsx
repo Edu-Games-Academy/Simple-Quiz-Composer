fc551909c9c423d3e4b8ae8c0ba1129627f7897c
Hieu Nguyen <isaactesla@gmail.com>
2023-03-04 15:58:07 +0700

    navbar placeholder

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { ReactComponent as DownloadIcon } from '@/assets/svg/download.svg';
 import { ReactComponent as UploadIcon } from '@/assets/svg/upload.svg';
 import { ReactComponent as UploadFileIcon } from '@/assets/svg/upload_file.svg';
 
-import ExportJsonButton from './ExportJsonButton';
 import NavItem from './NavItem';
 
 function NavBar() {
+  const importJson = () => {};
+  const importMoodle = () => {};
+  const exportJson = () => {};
   return (
     <nav className="fixed top-0 z-50 w-full border-b border-gray-700 bg-gray-800">
       <div className="container mx-auto flex flex-wrap items-center justify-between p-2">
@@ -21,11 +24,24 @@ function NavBar() {
           </span>
         </a>
         <div className="order-2 flex">
-          <ExportJsonButton />
+          <button
+            type="button"
+            className="mr-0 inline-flex items-center rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-800"
+            onClick={exportJson}
+          >
+            <DownloadIcon
+              aria-hidden="true"
+              className="mr-2 -ml-1 h-5 w-6"
+              fill="currentColor"
+              focusable="false"
+              role="img"
+            />
+            Export to JSON
+          </button>
         </div>
         <div className="order-1 flex w-auto items-center justify-between">
           <ul className="mt-0 flex flex-row space-x-8 rounded-lg border-0 bg-gray-800 p-4 text-sm font-medium">
-            <NavItem>
+            <NavItem onClick={importJson}>
               <UploadIcon
                 aria-hidden="true"
                 className="mr-2 -ml-1 h-4 w-4"
@@ -35,7 +51,7 @@ function NavBar() {
               />
               Import JSON
             </NavItem>
-            <NavItem>
+            <NavItem onClick={importMoodle}>
               <UploadFileIcon
                 aria-hidden="true"
                 className="mr-2 -ml-1 h-4 w-4"
