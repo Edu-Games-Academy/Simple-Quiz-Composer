23203ecb57300777764fed409585f056158739bf
Hieu Nguyen <isaactesla@gmail.com>
2023-04-21 05:42:09 +0000

    docs(ui): componets

--- sqc-ui-components/src/components/contexts/QuestionsContext.tsx
+++ sqc-ui-components/src/components/contexts/QuestionsContext.tsx
@@ -3,21 +3,25 @@ import { ReactNode, createContext, useContext, useState } from 'react'
 import { QuestionDispatchAction, useQuestionReducer } from '../../reducers/questionReducer'
 import { Question } from 'sqc-core-functions'
 
-type QuestionsProps = {
+export type QuestionsContextProps = {
   questions: Question[]
   questionsDispatch: React.Dispatch<QuestionDispatchAction>
   selectedQuestion: number
   setSelectedQuestion: React.Dispatch<React.SetStateAction<number>>
 }
 
-const QuestionsContext = createContext({} as QuestionsProps)
+const QuestionsContext = createContext({} as QuestionsContextProps)
 
+/**
+ * Hook to access question context
+ */
 const useQuestionsContext = () => useContext(QuestionsContext)
 
-type Props = {
-  children: ReactNode
-}
-function QuestionsProvider({ children }: Props) {
+/**
+ * Provider element to wrap the other elements to access question context
+ * @see {@link useQuestionsContext}
+ */
+function QuestionsProvider({ children }: { children: ReactNode }) {
   const { questions, questionsDispatch } = useQuestionReducer()
   const [selectedQuestion, setSelectedQuestion] = useState(0)
 
